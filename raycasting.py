e05745aa3f4bbe605cb7cf6f74e9ca470b9312b7
Peter Edwards <Techdread@gmail.com>
2023-10-31 10:40:43 +0000

    2D map, player raycast

---
+++ raycasting.py
@@ -0,0 +1,14 @@
+import pygame as pg
+import math
+from settings import *
+
+
+class RayCasting:
+    def __init__(self, game):
+        self.game = game
+
+    def ray_casting(self):
+        pass
+
+    def update(self):
+        self.ray_casting()
